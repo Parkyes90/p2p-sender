7efdcd894f4bf57caaf1fbbba81e1197cf4b725d
parkyes90 <parkyes90@gmail.com>
2022-10-02 13:22:34 +0900

    Add Websocket test

--- services/signaling-server/apps/download_channels/tests.py
+++ services/signaling-server/apps/download_channels/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from channels.testing import WebsocketCommunicator
+from config.asgi import application
+from django.test import TransactionTestCase
 
-# Create your tests here.
+
+class AppliesConsumerTestCase(TransactionTestCase):
+    def setUp(self) -> None:
+        self.url = f"/ws/download-channels/10/"
+        self.headers = [(b"origin", b"ws://localhost")]
+
+    async def test_with_valid_user(self):
+        """유효한 유저의 요청은 승인된다."""
+        communicator = WebsocketCommunicator(application, self.url, self.headers)
+        connected, _ = await communicator.connect()
+        self.assertTrue(connected)
+        await communicator.send_json_to({"type": "entrance", "message": "entrance"})
+        message = await communicator.receive_json_from()
+        self.assertEqual(message["type"], "entrance")
+        await communicator.disconnect()
