30d223ad9d4f1de24ad2b2412ed128a28b782887
parkyes90 <parkyes90@gmail.com>
2022-10-05 19:35:46 +0900

    use react-router

--- services/web-client/src/App.tsx
+++ services/web-client/src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Main</div>,
+  },
+  {
+    path: "download-files/:downloadId",
+    element: <div>download file</div>,
+  },
+]);
 
 function App() {
-  return <div className="App">Main</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
