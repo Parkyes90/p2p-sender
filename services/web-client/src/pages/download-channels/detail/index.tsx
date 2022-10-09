058253a8ec78e8a4c30b4fdd85f7f03ce6008eb3
parkyes90 <parkyes90@gmail.com>
2022-10-09 10:41:33 +0900

    apply websocket in detail page

--- services/web-client/src/pages/download-channels/detail/index.tsx
+++ services/web-client/src/pages/download-channels/detail/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
+import useWebSocket from "react-use-websocket";
+import { useWebsocketProtocol } from "../../../hooks/use-websocket-protocol";
 
 interface DownloadChannelDetailPageParams extends Record<string, string> {
   downloadId: string;
@@ -7,6 +9,13 @@ interface DownloadChannelDetailPageParams extends Record<string, string> {
 
 function DownloadChannelDetailPage() {
   const { downloadId } = useParams<DownloadChannelDetailPageParams>();
+  const websocketProtocol = useWebsocketProtocol();
+  const { lastMessage, readyState } = useWebSocket(
+    `${websocketProtocol}://${window.location.host}/ws/download-channels/${downloadId}/`
+  );
+  useEffect(() => {
+    console.log(lastMessage, readyState);
+  }, [lastMessage, readyState]);
   return <div>DownloadChannelDetailPage {downloadId}</div>;
 }
 
